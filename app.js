8e9675954a4c13ddf49f2f9ba9d3448b9371c6f4
manishkumar2654 <Hairbymanish@gmail.com>
2025-07-25 05:55:20 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
-
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res)=>{
-    res.send("okya");
-})
-
-app.get("/home", (req, res)=>{
-    res.send("this is home page")
-})
-
-app.get("/about", (req, res)=>{
-    res.send("about pagesssss")
-})
-
-app.get("/manish", (req, res)=>{
-    res.send("manish pagessss")
-})
+const userModel = require("./usermodel");
 
 
 
-app.listen(9000, ()=>{
-    console.log("server run on port 9000");
+app.get("/", (req, res) => {
+
+    res.send("hey");
+});
+
+app.get("/create", async (req, res)=>{
+    let createduser = await userModel.create({
+          name: "neha",
+    username: "nehaa",
+    email: "www.naii@gmail.com"
+    })
+   res.send(createduser);
     
+});
+
+
+app.get("/update", async (req, res)=>{
+   let updateuser = await userModel.findOneAndUpdate({username: "manish"}, {name: "manish kumar"}, {new: true})
+
+   res.send(updateuser);
+    
+});
+
+
+app.get("/read", async (req, res)=>{
+    let readuser = await userModel.find();
+    res.send(readuser);
 })
+app.get("/delete", async (req, res)=>{
+    let byuser = await userModel.findOneAndDelete({name: "mummy"});
+  
+    res.send(byuser);
+})
+
+
+app.listen(3000);
