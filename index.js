e86d2c730b25ac67d31462a239f183472c254d7c
manishkumar2654 <Hairbymanish@gmail.com>
2025-08-24 18:33:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
 const express = require("express");
-const app = express();
-require("dotenv").config();
-const port = process.env.PORT || 3000;
-
+const mongoose = require("mongoose");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/userRoutes");
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/manucrud")
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error("Mongo error:", err));
+
 app.use(cors());
-app.use(cookieParser());
+app.use(express.json());
 
-// Set Cookie
-app.get("/setcookie", (req, res) => {
-  res.cookie("name", "manish", { maxAge: 60 * 1000 });
-  res.cookie("course", "java");
-  res.cookie("fees", "5000");
-  res.send("Cookies have been sent");
-});
+app.use("/", userRoutes);
 
-// Display Cookie
-app.get("/displaycookie", (req, res) => {
-  const { name, course, fees } = req.cookies;
-  res.send({ name, course, fees });
-});
-
-// Delete Cookie
-app.get("/deletecookie", (req, res) => {
-  res.clearCookie("name");
-  res.clearCookie("course");
-  res.clearCookie("fees");
-  res.send("Cookies deleted");
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(7000, () => console.log("Server running on 7000"));
