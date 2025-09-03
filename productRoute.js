289369202446428ca38c177012d61fd756397a81
manishkumar2654 <Hairbymanish@gmail.com>
2025-09-03 13:16:49 +0530

    Add files via upload

--- productRoute.js
+++ productRoute.js
@@ -1,14 +1,18 @@
-// routes/productRoute.js
 const express = require("express");
 const route = express.Router();
-const ProductController = require("../controllers/ProductController");
+const ProductController= require("../controllers/ProductController")
 
-route.get("/homedisplay", ProductController.homeDisplay);
-route.get("/productdisplay", ProductController.productDisplay);
-route.get("/search", ProductController.searchProducts);
-route.get("/male", ProductController.getMaleProducts);
-route.get("/female", ProductController.getFemaleProducts);
-route.get("/kids", ProductController.getKidsProducts);
-route.get("/sale", ProductController.getSaleProducts);
+route.get("/homedisplay", ProductController.homeDisplay );
+route.get("/productdisplay", ProductController.productDisplay );
 
-module.exports = route;
+ 
+
+
+
+
+
+
+
+
+
+module.exports=route; 
