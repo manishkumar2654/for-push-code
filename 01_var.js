a9c13cda6da7c0acac10493981578745ad07998c
manishkumar2654 <Hairbymanish@gmail.com>
2025-09-06 09:57:22 +0530

    Add files via upload

---
+++ 01_var.js
@@ -0,0 +1,22 @@
+const accountId = 123456;
+let accountEmail = "manish@gmail.com";
+var accountPassword = 12345;
+accontCity = "jaipur";
+let accountState;
+
+//accountId = 2 not alowed
+
+console.table([accountId, accountEmail, accountPassword, accontCity,accountState]);
+
+
+accountEmail = "papa@gmail";
+accountPassword = 6987;
+accontCity = "bhopal";
+console.table([accountId, accountEmail, accountPassword, accontCity]);
+
+
+// curly brace scope hai    {Scope  }
+
+/*
+prefer not use var isme scope nahi hai
+*/
