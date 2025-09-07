5a8d1fbe9eec3a8d70b5eeb045c8b85363b75304
manishkumar2654 <Hairbymanish@gmail.com>
2025-09-07 20:07:03 +0530

    Add files via upload

---
+++ 02_datatype.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all js code as newer version
+
+//alert (3 + 3) // we are using node js not browser
+
+//mdn // ecma
+
+let foo = 42; // foo is now a number
+foo = "bar"; // foo is now a string
+foo = true; // foo is now a boolean
+
+const zoo = 42; // foo is a number
+const result = zoo + "1"; // JavaScript coerces foo to a string, so it can be concatenated with the other operand
+//console.log(result); // 421
+
+
+
+let name = "manish" //string
+let age = 10;
+let isLogin = false;
+let state ;
+
+
+//number => 2 to pwr 53
+//bigint => 
+    //string => "manish"
+//boolean => true or false
+//numll => stand alone value
+//undefine => 
+    //symbol => unique
+//undefined undefine me ata hai
+
+//object kya hai
+//null Object hai
+console.log(typeof undefined);
