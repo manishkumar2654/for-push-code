96577c05121b835a03ce7dc4109e955cdc896c9a
manishkumar2654 <Hairbymanish@gmail.com>
2025-09-08 14:29:36 +0530

    Add files via upload

---
+++ 03_conversionOperation.js
@@ -0,0 +1,45 @@
+let score = false
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let stringgchangnumber = Number(score)
+//console.log(typeof stringgchangnumber);
+//console.log( stringgchangnumber);
+
+let isLoggedIn = 1;
+
+let booleanisLogin = Boolean(isLoggedIn);
+//console.log(booleanisLogin);
+
+let someNumber = 33
+let stringnumber = String(someNumber);
+//console.log(stringnumber);
+//console.log(typeof (stringnumber));
+
+
+//****************opretion************************** */
+
+let value = 3
+let neg = -value
+//console.log(neg);
+
+
+let one = "hello"
+let two = " manish"
+let three = one + two;
+//console.log(three);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+//console.log(+true);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+
+
+
---
+++ 04_datatypeSummary.js
@@ -0,0 +1,21 @@
+ //array
+const heros = ["saktiman", "naagraj", "doga"];
+
+
+//objects
+let objs = {
+    name:"manish",
+    age: 22,
+}  
+
+
+//functions
+
+const myfun = function () {
+    console.log("hello world")
+};
+//console.log(typeof myfun);
+//console.log(myfun);
+
+myfun()
+9walai chsl rahi hai
