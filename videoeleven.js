5a8d80ff4d84b13e3c01e1e09b8251ae23276405
manishkumar2654 <Hairbymanish@gmail.com>
2025-09-09 14:53:23 +0530

    Add files via upload
    video eleven

---
+++ videoeleven.js
@@ -0,0 +1,38 @@
+// const name = "manish";
+// const repoCount = 50
+
+//console.log(name + repoCount + "golu")
+
+//console.log(`hello my name is ${name.toUpperCase()} and my repo is ${repoCount}`);
+
+const gameName = new String("imrann-akash");
+
+// console.log(gameName[2])
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("a"));
+
+
+// const newString = gameName.substring(0, 4)
+// //console.log(newString);
+const name = "mayank";
+//console.log(name.slice(2,3));
+
+const ekorstring = gameName.slice(2, 4);
+//console.log(ekorstring);
+
+const newstringsone = "   manish     "
+//console.log(newstringsone);
+//console.log(newstringsone.trim());
+
+
+
+const url = "https://hites.com/hitesh%20chodray";
+console.log(url.replace("%20", "_1"))
+
+console.log(url.includes("ramesh"));
+
+console.log(gameName.split( "-"));
