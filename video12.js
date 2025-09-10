81ff85b438a312586ad6ac90bf091de34c22211e
Manish kumar <Hairbymanish@gmail.com>
2025-09-10 13:54:14 +0530

    Add files via upload

---
+++ video12.js
@@ -0,0 +1,39 @@
+const score = 400
+
+const blance = new Number(100)
+//console.log(score)
+//console.log(blance)
+//console.log(blance.toString().length)
+//console.log(blance.toFixed(3));
+
+const onotherNuber = 23.556
+//console.log(onotherNuber.toPrecision(2));
+
+
+const hundred = 100000000;
+
+// console.log(hundred.toLocaleString("en-IN"));
+
+
+  /************maths*************************** */
+
+//   console.log(Math);
+//   console.log(Math.abs(-4)); 
+//   console.log(Math.round(4.6));
+//   console.log(Math.ceil(4.2));
+//   console.log(Math.floor(4.6));
+//   console.log(Math.min(4,3,6,4,));
+//   console.log(Math.max(4,3,6,4,));
+
+
+  console.log((Math.random()*10) + 1);
+  
+  
+  
+  const min = 10;
+  const max = 20 ;
+
+
+  console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+  console.log(Math.floor(Math.random() * 11 ) + 11);
+  
