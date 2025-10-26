12b40bef9e6efddfc2b8cf8169d70f892c9c41ee
Manish kumar <Hairbymanish@gmail.com>
2025-10-26 08:40:42 +0530

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Css/Home.css";
 
-createRoot(document.getElementById('root')).render(
-  <>
-    <App />
-  </>,
-)
+createRoot(document.getElementById("root")).render(
+  <App />
+);
