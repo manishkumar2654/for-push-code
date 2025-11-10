66a57c0304558d40b1b892cae108101a7d434414
Manish kumar <Hairbymanish@gmail.com>
2025-11-10 10:36:03 +0530

    Add files via upload

--- userModel.js
+++ userModel.js
@@ -1,10 +1,11 @@
-const mongoose = require("mongoose");
+const mongooser = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  userId: String,
-  name: String,
-  city: String,
-  password: String,
-});
+const userhai = new mongooser.Schema({
+    userid:Number,
+    name:String,
+    job:String,
+    password:String
+    
+})
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongooser.model("userschema", userhai);
