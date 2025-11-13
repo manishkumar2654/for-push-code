c06798c3f2a69450fd1b8ca6ccf655f5ae914958
Manish kumar <Hairbymanish@gmail.com>
2025-11-13 09:44:51 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,17 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./pages/Home";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import CreateUser from "./CreateUser";
+import Updateuser from "./Updateuser";
+import Users from "./Users";
+import Home from "./Home";
+import Display from "./Display";
+import Edit from "./Edit";
+import Search from "./search";
+
 
 const App = () => {
-  return (
+  return(
+    <>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home/>}> </Route>
+      <Route path="/create" element={<CreateUser/>}> </Route>
+      <Route path="/update" element={<Updateuser/>}> </Route>
+      <Route path="/user" element={<Users/>}></Route>
+      <Route path="/display" element={<Display/>}></Route>
+      <Route path="/edit/:id" element={<Edit/>}></Route>
+      <Route path="/search" element={<Search/>}></Route>
+    </Routes>
     </BrowserRouter>
-  );
-};
-
-export default App;
+    
+   
+    
+    </>
+  )
+}
+export default App
